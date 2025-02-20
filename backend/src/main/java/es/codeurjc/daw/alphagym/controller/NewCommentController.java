ebce5c46eb9d9b03fdf31f24d268a7483c5e60cb
CataUrjc <c.mazarache.2021@alumnos.urjc.es>
2025-02-20 21:06:20 +0100

    Added Mustache {{}} placeholders to comments.html and login.html to properly generate the final HTML output

--- backend/src/main/java/es/codeurjc/daw/alphagym/controller/NewCommentController.java
+++ backend/src/main/java/es/codeurjc/daw/alphagym/controller/NewCommentController.java
@@ -11,8 +11,10 @@ public class NewCommentController {
     public String comments(Model model, @RequestParam String title, @RequestParam String comment) {
         // Agregar el título y comentario al modelo para que se muestren en
         // comments.html
+        /* 
         model.addAttribute("title", title);
         model.addAttribute("comment", comment);
-        return "newComment"; // La página comments.html se va a cargar con los nuevos valores
+        */
+        return "newComment"; // La página comments.html se va a cargar con los nuevos valores   
     }
 }
