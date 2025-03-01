48867a9661f82ac3788c88668e9e4831ff455f88
CataUrjc <c.mazarache.2021@alumnos.urjc.es>
2025-03-01 23:47:18 +0100

    Commented check logged user

--- backend/src/main/java/es/codeurjc/daw/alphagym/controller/TrainingCommentController.java
+++ backend/src/main/java/es/codeurjc/daw/alphagym/controller/TrainingCommentController.java
@@ -12,8 +12,32 @@ import es.codeurjc.daw.alphagym.service.UserService;
 
 
 @Controller
-public class TrainingCommentController {
-     
+public class TrainingCommentController { 
+      
+    /*
+    @ModelAttribute("user")
+    public void addAttributes(Model model, HttpServletRequest request){
+
+        Principal principal = request.getUserPrincipal();
+
+        if (principal != null) {
+            Optional <User> user = userService.findByEmail(principal.getName()); //se usa getName porque asi se hace desde security
+            if (user.isPresent()){
+                if (user.get().isRole("USER")){
+                    model.addAttribute("user", true);
+                }
+                if (user.get().isRole("ADMIN")){
+                    model.addAttribute("admin", true);
+                }
+            }
+            model.addAttribute("logged", true);
+            model.addAttribute("userName", principal.getName());
+        } else {
+            model.addAttribute("logged", false);
+        }
+    }
+    */
+    
     @Autowired
     private UserService userService;
     @Autowired
@@ -37,6 +61,15 @@ public class TrainingCommentController {
     public String showComment(Model model, @PathVariable Long trainingId, @PathVariable Long commentId){
         return "comment";
     }
+    /*
+    @PostMapping("/trainingComments/{trainingId}")
+    public String createComment(Model model,@PathVariable Long trainingId){
+        model.addAttribute("comment", trainingCommentService.getAllTrainingComments(trainingId));
+        return "comments";
+    }
+    */
+
+
 
 
 }
