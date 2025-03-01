942a9f2797f83fb290756dc7b8e3262b44cc2eba
victorcc02 <117302575+victorcc02@users.noreply.github.com>
2025-03-01 17:19:30 +0100

    fix showDiet view error
    Co-Authored-By: aadri-2003 <117304862+aadri-2003@users.noreply.github.com>

--- backend/src/main/java/es/codeurjc/daw/alphagym/controller/NutritionController.java
+++ backend/src/main/java/es/codeurjc/daw/alphagym/controller/NutritionController.java
@@ -54,7 +54,7 @@ public class NutritionController {
             model.addAttribute("userId",user.getId());
             return "detailsNutrition";
         }*/
-        return "redirect:/showDiet";
+        return "showDiet";
     }
 
     
