a904c757afd4dd8fa44dc04918c9609ac31bc706
XdeXavi <medinaxavi.22@gmail.com>
2025-04-27 11:46:58 +0200

    Update user.dto.ts

--- frontend/src/app/dto/user.dto.ts
+++ frontend/src/app/dto/user.dto.ts
@@ -2,4 +2,17 @@ export interface UserDTO {
     id?: number;
     name: string;
     roles: string[];
+    email: string;
+    password: string;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface RegisterRequest {
+  name: string;
+  email: string;
+  password: string;
 }
