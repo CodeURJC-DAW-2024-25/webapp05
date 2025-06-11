2205ff2b2b8a4640028de6d3a8c667536edf63c0
victorcc02 <117302575+victorcc02@users.noreply.github.com>
2025-06-11 19:11:03 +0200

    Update SPAController.java

--- backend/src/main/java/es/codeurjc/daw/alphagym/controller/web/SPAController.java
+++ backend/src/main/java/es/codeurjc/daw/alphagym/controller/web/SPAController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SPAController {
-    @GetMapping({"/spa", "/spa/**/{path:[^\\.]*}", "/{path:spa[^\\.]*}", "/spa/", "/new", "/new/**/{path:[^\\.]*}", "/{path:spa[^\\.]*}"})
+    @GetMapping({"/spa", "/spa/**/{path:[^\\.]*}", "/{path:spa[^\\.]*}", "/spa/", "/new", "/new/", "/new/**/{path:[^\\.]*}", "/{path:spa[^\\.]*}"})
     public String redirect() {
 
         return "forward:/spa/index.html";
